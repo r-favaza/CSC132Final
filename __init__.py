84eccd3b7d721a1280ade94ad69a6af59c267ce3
r-favaza <112298777+r-favaza@users.noreply.github.com>
2023-04-24 10:44:58 -0500

    update

--- __init__.py
+++ __init__.py
@@ -1,7 +1,11 @@
 import floor
 import project
 import spreadsheet
+import lights
 
+<<<<<<< HEAD
+=======
 projects = spreadsheet.returnProjects()
 
 print(projects)
+>>>>>>> 0d568ce7e2d3f8c8860a19bfe839ee5dd5d69212
---
+++ lights.py
@@ -0,0 +1,11 @@
+import gui
+import RPi.GPIO as GPIO
+
+#GPIO SETUP
+GPIO.setmode(GPIO.BCM)
+
+GPIO.setup(17, GPIO.OUT) #set LED to pin 17
+
+
+if Ui_Dialog. == "floor 1":
+    
