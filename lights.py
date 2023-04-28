138948f40b2ba0e379cc298a6ebe683e611e3d6d
r-favaza <112298777+r-favaza@users.noreply.github.com>
2023-04-27 22:00:46 -0500

    allow changes

--- lights.py
+++ lights.py
@@ -1,10 +1,48 @@
 import gui
 import RPi.GPIO as GPIO
+import pyqt6
 
 #GPIO SETUP
 GPIO.setmode(GPIO.BCM)
+'''
+LEDs Index
+0 = 1st Floor
+1 = 2nd floor
+2 = 3rd floor
+3 = 
+4 = 
+5 = 
+6 = 
+7 = 
+8 = 
+9 = 
+10 = 
+11 = 
+13 = 
+14 = 
+15 = 
+16 = 
+17 = 
+18 = 
+19 = 
+20 = 
+21 = 
+'''
+LEDs = []
+for i in range(1, 23):
+    LEDs.append(GPIO.setup(i, GPIO.OUT)) 
 
+<<<<<<< HEAD
+while True:
+    gui.Ui_Dialog.update()
+    #if pyqt6.currentIndex() == 1:
+     #   for j in range(21):
+      #      GPIO.output(LEDs[j], 1)
+=======
+>>>>>>> c4ef95fe22fe0ef37e58b7dea09dac132cc5501f
 
+<<<<<<< HEAD
+=======
 lights = []
 
 floor1 = None; lights.append(floor1)
@@ -23,6 +61,7 @@ def allOff():
         GPIO.output(lights, False)
 
 if gui.ui.listTabWidget.currentIndex() == 2:
+>>>>>>> c4ef95fe22fe0ef37e58b7dea09dac132cc5501f
     
     if gui.ui.tabWidget_5.currentIndex() == 0:
         allOff()
