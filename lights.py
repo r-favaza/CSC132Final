d4c0c269dec09a5928d94a1ed7c52a14e2ac1ea4
r-favaza <112298777+r-favaza@users.noreply.github.com>
2023-04-27 22:05:10 -0500

    set pins on lights

--- lights.py
+++ lights.py
@@ -4,52 +4,18 @@ import pyqt6
 
 #GPIO SETUP
 GPIO.setmode(GPIO.BCM)
-'''
-LEDs Index
-0 = 1st Floor
-1 = 2nd floor
-2 = 3rd floor
-3 = 
-4 = 
-5 = 
-6 = 
-7 = 
-8 = 
-9 = 
-10 = 
-11 = 
-13 = 
-14 = 
-15 = 
-16 = 
-17 = 
-18 = 
-19 = 
-20 = 
-21 = 
-'''
+
 LEDs = []
 for i in range(1, 23):
     LEDs.append(GPIO.setup(i, GPIO.OUT)) 
 
-<<<<<<< HEAD
-while True:
-    gui.Ui_Dialog.update()
-    #if pyqt6.currentIndex() == 1:
-     #   for j in range(21):
-      #      GPIO.output(LEDs[j], 1)
-=======
->>>>>>> c4ef95fe22fe0ef37e58b7dea09dac132cc5501f
-
-<<<<<<< HEAD
-=======
 lights = []
 
-floor1 = None; lights.append(floor1)
-floor2 = None; lights.append(floor2)
-floor3 = None; lights.append(floor3)
-section1 = None; lights.append(section1)
-section2 = None; lights.append(section2)
+floor1 = 4; lights.append(floor1)
+floor2 = 12; lights.append(floor2)
+floor3 = 16; lights.append(floor3)
+section1 = 18; lights.append(section1)
+section2 = 22; lights.append(section2)
 
 for i in lights:
     GPIO.setup(lights, GPIO.OUT)
@@ -61,7 +27,6 @@ def allOff():
         GPIO.output(lights, False)
 
 if gui.ui.listTabWidget.currentIndex() == 2:
->>>>>>> c4ef95fe22fe0ef37e58b7dea09dac132cc5501f
     
     if gui.ui.tabWidget_5.currentIndex() == 0:
         allOff()
