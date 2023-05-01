eed76b01b34d0aff054cba20b51059ca11f894af
cdw108 <127534441+cdw108@users.noreply.github.com>
2023-05-01 09:28:07 -0500

    lights

--- lights.py
+++ lights.py
@@ -32,36 +32,12 @@ LEDs = []
 for i in range(1, 23):
     LEDs.append(GPIO.setup(i, GPIO.OUT)) 
 
-<<<<<<< HEAD
 while True:
     gui.Ui_Dialog.update()
     #if pyqt6.currentIndex() == 1:
      #   for j in range(21):
       #      GPIO.output(LEDs[j], 1)
-=======
->>>>>>> c4ef95fe22fe0ef37e58b7dea09dac132cc5501f
 
-<<<<<<< HEAD
-=======
-lights = []
-
-floor1 = None; lights.append(floor1)
-floor2 = None; lights.append(floor2)
-floor3 = None; lights.append(floor3)
-section1 = None; lights.append(section1)
-section2 = None; lights.append(section2)
-
-for i in lights:
-    GPIO.setup(lights, GPIO.OUT)
-
-#GPIO.setup(17, GPIO.OUT) #set LED to pin 17
-
-def allOff():
-    for i in lights:
-        GPIO.output(lights, False)
-
-if gui.ui.listTabWidget.currentIndex() == 2:
->>>>>>> c4ef95fe22fe0ef37e58b7dea09dac132cc5501f
     
     if gui.ui.tabWidget_5.currentIndex() == 0:
         allOff()
